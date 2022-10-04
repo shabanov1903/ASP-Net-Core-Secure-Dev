c2a2154eed939ada7fe9e8715bf2d1a8e55be547
Danil Shabanov <shabanov1903@gmail.com>
2022-10-04 17:58:50 +0300

    Typical security model. Assembly signature

---
+++ CardStorageService/CardStorageService.Core/Models/DTO/AuthenticationRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardStorageService.Core.Models.DTO
+{
+    public class AuthenticationRequestDTO
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
--- CardStorageService/CardStorageService.Core/Models/DTO/ClientDTO.cs
+++ CardStorageService/CardStorageService.Core/Models/DTO/ClientDTO.cs
@@ -1,10 +1,13 @@
-﻿namespace CardStorageService.DTO
+﻿namespace CardStorageService.Core.Models.DTO
 {
-    public class ClientDTO
+    public class ClientDTO : IError
     {
         public int? ClientId { get; set; }
         public string? Surname { get; set; }
         public string? FirstName { get; set; }
         public string? Patronymic { get; set; }
+
+        public int? Code { get; set; }
+        public string? Message { get; set; }
     }
 }
