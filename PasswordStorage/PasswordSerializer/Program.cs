cb00abda70f28da8909d48bf640288e3ab51732e
Danil Shabanov <shabanov1903@gmail.com>
2022-10-05 21:16:17 +0300

    Serializing passwords in binary format

---
+++ PasswordStorage/PasswordSerializer/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PasswordSerializer.Models;
+using PasswordSerializer.Utils;
+
+var conectionStringCardStorage = new ConnectionString
+{
+    DatabaseName = "CardStorageDB",
+    Host = "localhost",
+    Password = "qwwerty123",
+    UserName = "admin"
+};
+
+var conectionStringPasswordStorage = new ConnectionString
+{
+    DatabaseName = "PasswordStorageDB",
+    Host = "localhost",
+    Password = "12345678",
+    UserName = "user"
+};
+
+List<ConnectionString> connections = new List<ConnectionString>();
+connections.Add(conectionStringCardStorage);
+connections.Add(conectionStringPasswordStorage);
+
+Serializer serializer= new();
+serializer.Serialize(connections);
+
+connections = serializer.Deserialize();
+
+foreach (var connection in connections)
+    Console.WriteLine(connection);
+
+Console.ReadKey(true);
